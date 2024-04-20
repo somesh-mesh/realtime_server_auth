249ac47692bb718cb3146edd4212cd128f5bceb1
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-04-20 12:55:07 +0530

    Server

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+app.use(express.json());
+
+const ALL_USERS = [
+  {
+    username: "somesh@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+
+function userExists(username, password) {
+  let exists = false;
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (
+      ALL_USERS[i].username === username &&
+      ALL_USERS[i].password === password
+    ) {
+      exists = true;
+      break; // Once found, no need to continue loop
+    }
+  }
+  return exists;
+}
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesn't exist in our in-memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // Logic to return a list of users other than this username
+    const otherUsers = ALL_USERS.filter(user => user.username !== username);
+    return res.json(otherUsers);
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
